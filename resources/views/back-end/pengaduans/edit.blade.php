99d8248e55ebdaa96dcc89dad5bf287cafdd7fd7
Ryan Prabowo, S.T <ryanprabowo96@gmail.com>
2024-10-09 13:53:29 +0800

    updating verifikasi name

--- resources/views/back-end/pengaduans/edit.blade.php
+++ resources/views/back-end/pengaduans/edit.blade.php
@@ -5,12 +5,12 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="py-12 ">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
 
                 <body>
-                    <h2>Verifikasi Pengaduan</h2>
+                    {{-- <h2>Verifikasi Pengaduan</h2> --}}
                     <form action="{{ route('pengaduans.update', $pengaduan->id) }}" method="POST" class="max-w-sm mx-auto">
                         @csrf
                         @method('PUT')
--- resources/views/back-end/pengaduans/index.blade.php
+++ resources/views/back-end/pengaduans/index.blade.php
@@ -15,9 +15,9 @@
                             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                     <tr>
-                                        <th scope="col" class="px-6 py-3">
+                                        {{-- <th scope="col" class="px-6 py-3">
                                             Nama
-                                        </th>
+                                        </th> --}}
                                         <th scope="col" class="px-6 py-3">
                                             Nomor HP
                                         </th>
@@ -41,7 +41,7 @@
                                 <tbody>
                                     @foreach ($pengaduans as $pengaduan)
                                     <tr>
-                                        <td class="px-6 py-4">{{ $pengaduan->user->name }}</td>
+                                        {{-- <td class="px-6 py-4">{{ $pengaduan->user->name }}</td> --}}
                                         
                                         <td class="px-6 py-4">{{ $pengaduan->nomor_hp }}</td>
                                         <td class="px-6 py-4">{{ $pengaduan->perihal }}</td>
